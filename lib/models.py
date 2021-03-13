d40f702cfa2584e2eb08d4eaf8e9f6a6f32c740b
jessecross <jesse.cross17@imperial.ac.uk>
2021-03-13 22:17:42 +0000

    Refashioned Bowman/Hills models to separate T21 and Tfg models. Can be sampled individually or combined.

--- lib/models.py
+++ lib/models.py
@@ -18,37 +18,32 @@ Contact: yi.lim17@imperial.ac.uk
 ####################################################
 import numpy as np
 
+
+
 ####################################################
-##################### MODELS #######################
+################## BOWMAN (2018) ###################
 ####################################################
-def mock(nu, A, nu0, w, tau):
+# Bowman (2018) 21 cm Signal Model (Flattened Gaussian Profile)
+def flattened_gaussian(nu, A, nu0, w, tau):
     """
-    Flattened Gaussian 21cm and Linearised Foreground
-    Model as in Bowman (2018). Foreground signal - Equation (2). 21 cm signal - Equations (5) and (6).
-    Also as in Hills (2018). Foreground signal - Equation (8). 21 cm signal - Equations (2) and (3).
+    Flattened Gaussian.
+    As in Bowman (2018) equations (5) and (6).
+    As in Hills (2018) equations (2) and (3).
     """
-    
-    # 21 cm Signal Model (Flattened Gaussian Profile)
+
     B = (4.0 * np.power((nu - nu0), 2.0) / np.power(w, 2.0)) * np.log(-np.log((1.0 + np.exp(-tau))/2.0) / tau)
     T21 = - A * (1.0 - np.exp(-tau * np.exp(B))) / (1.0 - np.exp(-tau))
-    
-    Tsky = T21
 
-    return Tsky
+    return T21
 
-
-def linearised_model(nu, A, nu0, w, tau, a0, a1, a2, a3, a4):
+# Bowman (2018) Foreground Signal Model (Linearised Emission)
+def linearised_foreground(nu, a0, a1, a2, a3, a4):
     """
-    Flattened Gaussian 21cm and Linearised Foreground
-    Model as in Bowman (2018). Foreground signal - Equation (2). 21 cm signal - Equations (5) and (6).
-    Also as in Hills (2018). Foreground signal - Equation (8). 21 cm signal - Equations (2) and (3).
+    Linearised Foreground.
+    As in Bowman (2018) equation (2).
+    As in Hills (2018) equation (8).
     """
-    
-    # 21 cm Signal Model (Flattened Gaussian Profile)
-    B = (4.0 * np.power((nu - nu0), 2.0) / np.power(w, 2.0)) * np.log(-np.log((1.0 + np.exp(-tau))/2.0) / tau)
-    T21 = - A * (1.0 - np.exp(-tau * np.exp(B))) / (1.0 - np.exp(-tau))
-    
-    # Foreground Signal Model (Linearised emission)
+
     nuc = 75.0          # Foreground central frequency as specified in paper
     x = nu / nuc        # Normalised frequency terms
 
@@ -58,23 +53,44 @@ def linearised_model(nu, A, nu0, w, tau, a0, a1, a2, a3, a4):
     Tfg += a3 * np.power(x, -4.5)
     Tfg += a4 * np.power(x, -2.0)
 
+    return Tfg
+
+# Bowman (2018) Combined Model (Linearised Foreground with Flattened Gaussian Signal)
+def linearised_model(nu, A, nu0, w, tau, a0, a1, a2, a3, a4):
+    """
+    Bowman (2018) and Hills (2018) Linearised Model with Flattened Gaussian
+    """
+
+    T21 = flattened_gaussian(nu, A, nu0, w, tau)
+    Tfg = linearised_foreground(nu, a0, a1, a2, a3, a4)
+
     # Combined signal
     Tsky = T21 + Tfg
 
     return Tsky
 
 
-def systematic_model(nu, A, phi, l, a0, a1, a2, a3, a4, a5):
+
+####################################################
+################### HILLS (2018) ###################
+####################################################
+# Hills (2018) 21 cm Signal Model (Sinusoidal)
+def sinusoidal(nu, A, phi, l):
     """
-    Flattened Gaussian 21cm and 5-term Polynomial Foreground.
-    Model as in Hills (2018). Foreground signal - Equation (10). 21 cm signal - Equation within Table 1.
+    Sine Wave. Table 1 in Hills (2018).
     """
 
-    # 21 cm Signal (Sinusoidal)
     y = ((2.0 * np.pi * nu)/l) + phi 
     T21 = A * np.sin(y)
 
-    # Foreground signal
+    return T21
+
+# Hills (2018) Foreground Signal Model (5-term Polynomial)
+def five_polynomial(nu, a0, a1, a2, a3, a4, a5):
+    """
+    5-term Polynomial Foreground as in Hills (2018) equation (10).
+    """
+    
     nuc = 75.0          # Foreground central frequency as specified in Hills (2018)
     x = nu / nuc        # Normalised frequency terms
 
@@ -84,6 +100,17 @@ def systematic_model(nu, A, phi, l, a0, a1, a2, a3, a4, a5):
     Tfg += a3 * np.power(x,  0.5) 
     Tfg += a4 * np.power(x,  1.5)
     Tfg += a5 * np.power(x,  2.5)
+
+    return Tfg
+
+# Hills (2018) Combined Model (5-term Polynomial Foreground with Sinusoidal Signal)
+def systematic_model(nu, A, phi, l, a0, a1, a2, a3, a4, a5):
+    """
+    Hills (2018) 5-term Polynomial Foreground with Sinusoidal Signal
+    """
+
+    T21 = sinusoidal(nu, A, phi, l)
+    Tfg = five_polynomial(nu, a0, a1, a2, a3, a4, a5)
 
     # Combined signal
     Tsky = T21 + Tfg
